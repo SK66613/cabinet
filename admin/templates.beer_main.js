699b885c6c3cbdbba9e0a2475d0ce7e8fe2ca786
SK66613 <cyberian13@icloud.com>
2025-12-29 14:48:11 +0300

    Delete admin directory

--- admin/templates.beer_main.js
+++
@@ -1,1 +0,0 @@
-window.BUILTIN_TEMPLATES=window.BUILTIN_TEMPLATES||{};window.BUILTIN_TEMPLATES['Beer Main']={sections:[]};
--- admin/templates.js
+++
@@ -1,1 +0,0 @@
-window.BUILTIN_TEMPLATES=window.BUILTIN_TEMPLATES||{};window.BUILTIN_TEMPLATES['Demo Main']={sections:[]};window.BUILTIN_TEMPLATES['SalesGenius Main']={sections:[]};
